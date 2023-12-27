79b248278046e42b4b04804b09d89f0ea87c90dd
Ашот Свазян <ashot.svazyan222@gmail.com>
2023-12-27 23:32:11 +0300

    update frontend

--- frontend/src/scripts/applicant/model.js
+++ frontend/src/scripts/applicant/model.js
@@ -27,4 +27,14 @@ export default function () {
     openModel('.security__login', '.model__login')
     openModel('.security__email', '.model__email')
 
+    document.querySelector('.lk__nav-chat > span')?.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        document.querySelector('.lk__chat-pop').classList.toggle('lk__chat-block')
+    })
+
+    document.querySelectorAll('.lk__button-wrap > button')?.forEach(el => {
+        el.addEventListener('click', () => el.nextElementSibling.classList.toggle('lk__button-menu-show'))
+    })
+
 }
