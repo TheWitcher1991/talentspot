a8be4a46b1751ef0998548dbb742fa190b20f3b2
Kassir322 <kassir3222@gmail.com>
2024-01-05 23:51:52 +0300

    Revert "начало работы с applicant"
    This reverts commit 872956968d40f70551d9853301252890d81dbfb8.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-import React, { lazy, Suspense, useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, {lazy, Suspense, useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Loader from './components/Loader'
 import './styles/index.scss'
-const Dashboard = lazy(() => import('./pages/applicant/Dashboard'))
 
 const BaseLayout = lazy(() => import('./pages/core/layout/BaseLayout'))
 const Home = lazy(() => import('./pages/core/Home'))
@@ -12,12 +11,8 @@ const Resume = lazy(() => import('./pages/core/Resume'))
 const About = lazy(() => import('./pages/core/About'))
 const Automation = lazy(() => import('./pages/core/Automation'))
 
-const AuthLayoutWithIcon = lazy(() =>
-	import('./pages/auth/layout/AuthLayoutWithIcon')
-)
-const AuthLayoutWithoutIcon = lazy(() =>
-	import('./pages/auth/layout/AuthLayoutWithoutIcon')
-)
+const AuthLayoutWithIcon = lazy(() => import('./pages/auth/layout/AuthLayoutWithIcon'))
+const AuthLayoutWithoutIcon = lazy(() => import('./pages/auth/layout/AuthLayoutWithoutIcon'))
 const Recovery = lazy(() => import('./pages/auth/Recovery'))
 const Signup = lazy(() => import('./pages/auth/Signup'))
 const Login = lazy(() => import('./pages/auth/Login'))
@@ -39,97 +34,99 @@ const Feedback = lazy(() => import('./pages/core/support/Feedback'))
 
 const NotFound = lazy(() => import('./components/NotFound'))
 
-export default function App() {
-	const [user, setUser] = useState(3)
-	// 1 - гость
-	// 2 - соискатель
-	// 3 - кандидат
+export default function App () {
+    const [user, setUser] = useState(1)
+    // 1 - гость
+    // 2 - соискатель
+    // 3 - работодатель
 
-	// Установка прокрутки страницы на 0 при смене маршрута
-	useEffect(() => {
-		document.documentElement.scrollTop = 0
-		document.scrollingElement.scrollTop = 0
-	}, [])
+    // Установка прокрутки страницы на 0 при смене маршрута
+    useEffect(() => {
+        document.documentElement.scrollTop = 0
+        document.scrollingElement.scrollTop = 0
+    }, [])
 
-	/**
-	 * Все три имеют доступ к страницам, которые находятся в pages/core
-	 * В зависимости типа пользователя будет соответствующий header, footer и т.д.
-	 *
-	 * Стартовая страница для гостя: /
-	 * Стартовая страница для соискателя: applicant/dashboard
-	 * Стартовая страница для работодателя: employer/dashboard
-	 *
-	 * TODO: Сделать файл routes.js
-	 */
+    /**
+     * Все три имеют доступ к страницам, которые находятся в pages/core
+     * В зависимости типа пользователя будет соответствующий header, footer и т.д.
+     *
+     * Стартовая страница для гостя: /
+     * Стартовая страница для соискателя: applicant/dashboard
+     * Стартовая страница для работодателя: employer/dashboard
+     *
+     * TODO: Сделать файл routes.js
+     */
 
-	return (
-		<main id="wrapper" className="wrapper">
-			<Router>
-				<Suspense fallback={<Loader />}>
-					<Routes>
-						{/** <BASE_ROUTER> **/}
-						<Route path="/" element={<BaseLayout />}>
-							<Route index element={<Home />} />
-							<Route path="vacancy/:id" element={<Vacancy />} />
-							<Route path="company/:id" element={<Company />} />
-							<Route path="resume/:id" element={<Resume />} />
-							<Route path="about" element={<About />} />
-							<Route path="automation" element={<Automation />} />
-						</Route>
+    return (
+        <main id='wrapper' className='wrapper'>
+            <Router>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        {/** <BASE_ROUTER> **/}
+                        <Route path='/' element={<BaseLayout />} >
+                            <Route index element={<Home/>} />
+                            <Route path='vacancy/:id' element={<Vacancy/>}  />
+                            <Route path='company/:id' element={<Company/>}  />
+                            <Route path='resume/:id' element={<Resume/>}  />
+                            <Route path='about'  element={<About/>} />
+                            <Route path='automation' element={<Automation />} />
+                        </Route>
 
-						<Route path="/search" element={<SearchLayout />}>
-							{/** ПОИСК **/}
-							<Route path="vacancy" element={<SearchVacancy />} />
-							<Route path="company" element={<SearchCompany />} />
-							<Route path="resume" element={<SearchResume />} />
-							{/** РАСШИРЕННЫЕ ПОИСК **/}
-							<Route path="expanded">
-								<Route path="vacancy" />
-								<Route path="company" />
-								<Route path="resume" />
-							</Route>
-						</Route>
+                        <Route path='/search' element={<SearchLayout />}>
+                            {/** ПОИСК **/}
+                            <Route path='vacancy' element={<SearchVacancy />} />
+                            <Route path='company' element={<SearchCompany />} />
+                            <Route path='resume' element={<SearchResume />} />
+                            {/** РАСШИРЕННЫЕ ПОИСК **/}
+                            <Route path='expanded'>
+                                <Route path='vacancy'  />
+                                <Route path='company'  />
+                                <Route path='resume'  />
+                            </Route>
+                        </Route>
 
-						<Route path="/support" element={<SupportLayout />}>
-							{/** ЦЕНТР ПОДДЕРЖКИ **/}
-							<Route index element={<Support />} />
-							<Route path="conditions" element={<Conditions />} />
-							<Route path="feedback" element={<Feedback />} />
-							<Route path="legal" element={<Legal />} />
-							<Route path="offer" element={<Offer />} />
-							<Route path="policy" element={<Policy />} />
-							<Route path="promo" element={<Promo />} />
-							<Route path="terms" element={<Terms />} />
-						</Route>
-						{/** </BASE_ROUTER> **/}
+                        <Route path='/support' element={<SupportLayout />}>
+                            {/** ЦЕНТР ПОДДЕРЖКИ **/}
+                            <Route index element={<Support />} />
+                            <Route path='conditions' element={<Conditions />} />
+                            <Route path='feedback' element={<Feedback />} />
+                            <Route path='legal' element={<Legal />} />
+                            <Route path='offer' element={<Offer />} />
+                            <Route path='policy' element={<Policy />} />
+                            <Route path='promo' element={<Promo />} />
+                            <Route path='terms' element={<Terms />} />
+                        </Route>
+                        {/** </BASE_ROUTER> **/}
 
-						{/** С этим че делать? **/}
-						<Route path="/logout" />
+                        {/** С этим че делать? **/}
+                        <Route path='/logout'  />
 
-						{user === 1 && (
-							<>
-								<Route path="/" element={<AuthLayoutWithIcon />}>
-									<Route path="/login" element={<Login />} />
-									<Route path="/signup" element={<Signup />} />
-								</Route>
-								<Route path="/" element={<AuthLayoutWithoutIcon />}>
-									<Route path="/recovery" element={<Recovery />} />
-								</Route>
-							</>
-						)}
+                        {user === 1 && (
+                            <>
+                                <Route path='/' element={<AuthLayoutWithIcon />}>
+                                    <Route path='/login' element={<Login />} />
+                                    <Route path='/signup' element={<Signup />} />
+                                </Route>
+                                <Route path='/' element={<AuthLayoutWithoutIcon />}>
+                                    <Route path='/recovery' element={<Recovery />} />
+                                </Route>
+                            </>
+                        )}
 
-						{user === 2 && <Route path="/employer"></Route>}
+                        {user === 2 && (
+                            <Route path='/employer'>
+                            </Route>
+                        )}
 
-						{user === 3 && (
-							<Route path="/applicant">
-								<Route path="dashboard" element={<Dashboard />} />
-							</Route>
-						)}
+                        {user === 3 && (
+                            <Route path='/applicant'>
+                            </Route>
+                        )}
 
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</Suspense>
-			</Router>
-		</main>
-	)
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+                </Suspense>
+            </Router>
+        </main>
+    )
 }
